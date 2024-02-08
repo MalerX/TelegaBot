d36c9073aff2a7977a1d1cff9572957669b52670
Vladimir Shevchenko <vshevchenko@inpromconsult.ru>
2024-02-08 20:42:24 +0300

    add vk api

--- src/main/java/com/malerx/bot/factory/BeanFactory.java
+++ src/main/java/com/malerx/bot/factory/BeanFactory.java
@@ -5,8 +5,13 @@ import com.malerx.bot.handlers.commands.impl.CustomBodyHandler;
 import com.malerx.bot.services.exchange.Exchange;
 import com.malerx.bot.services.weather.GeoData;
 import com.malerx.bot.services.weather.WeatherData;
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Value;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
@@ -15,9 +20,15 @@ import java.util.concurrent.Executors;
 @Factory
 public class BeanFactory {
     private final ObjectMapper mapper;
+    private final Long groupId;
+    private final String vkToken;
 
-    public BeanFactory(ObjectMapper mapper) {
+    public BeanFactory(ObjectMapper mapper,
+                       @Value(value = "${api.vk.access_token}") String vkToken,
+                       @Value(value = "${api.vk.group_id}") Long groupId) {
         this.mapper = mapper;
+        this.vkToken = vkToken;
+        this.groupId = groupId;
     }
 
     @Bean
@@ -40,5 +51,16 @@ public class BeanFactory {
     @Bean
     public HttpResponse.BodyHandler<GeoData> geoDataBodyHandler() {
         return new CustomBodyHandler<>(mapper, GeoData.class);
+    }
+
+    @Bean
+    public VkApiClient vkClient() {
+        TransportClient transportClient = new HttpTransportClient();
+        return new VkApiClient(transportClient);
+    }
+
+    @Bean
+    public UserActor groupActor() {
+        return new UserActor(groupId, vkToken);
     }
 }
