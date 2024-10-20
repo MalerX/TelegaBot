e99f94ad17d44203d4d72953bd9b48ba1f3d9135
vshevchenko <vshevchenko@inpromconsult.ru>
2024-10-20 13:15:53 +0300

    fix exchange message

--- src/main/java/com/malerx/bot/services/exchange/ExchangeService.java
+++ src/main/java/com/malerx/bot/services/exchange/ExchangeService.java
@@ -50,7 +50,7 @@ public class ExchangeService {
             return message(update, "Не удалось загрузить курсы валют");
         Double currentCourse = currencies.getOrDefault(currency.toUpperCase(), 0.0);
         Double exchanged = money * currentCourse;
-        String message = String.format("Обмен по курсу %.3f -- %.3f", currentCourse, exchanged);
+        String message = String.format("%.3f RUB = %.3f %s", money, exchanged, currency);
         return message(update, message);
     }
 
